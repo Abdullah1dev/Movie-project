f074819694af25ee029ccff399a21320899f20b9
Abdullah1dev <242677@students.au.edu.pk>
2025-11-30 19:19:12 +0500

    create the urls.py file of app

---
+++ watchlist/urls.py
@@ -0,0 +1,11 @@
+from django.urls  import path
+from . import views
+ 
+#now create the urls for wathlist app
+
+urlpatterns = [
+    path('',views.movie_list,name='movie_list'),
+    path('add_movie/',views.add_movie,name='add_movie'),
+    path('watched/<int:movie_id>/',views.mark_watched,name='mark_watched'),
+    path('rate/<int:movie_id>/',views.rate_movie,name="rate_movie"),
+]
