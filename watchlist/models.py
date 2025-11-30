a6db442a92e0da708d07a5517d742e7c73d7afc5
Abdullah1dev <242677@students.au.edu.pk>
2025-11-30 18:34:07 +0500

    create the models of app

--- watchlist/models.py
+++ watchlist/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title=models.CharField(max_length=150)
+    year=models.IntegerField(null=True , blank=True)
+    genre=models.CharField(max_length=100 , blank=True)
+    watched=models.BooleanField(default=False)
+    rating=models.IntegerField(null=True , blank=True)
+    notes=models.TextField(max_length=300 , blank=True)
+    added_date=models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering =['-added_date']
+        
+      
