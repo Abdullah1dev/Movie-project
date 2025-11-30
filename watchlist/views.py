7c71348a5738d685d860c6555b32df1024036b82
Abdullah1dev <242677@students.au.edu.pk>
2025-11-30 19:07:18 +0500

    create views file for app

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , get_object_or_404
+#now we import the models class here
+from .models import Movie
 
-# Create your views here.
+def movie_list(request):
+    movies=Movie.objects.all()
+    return render(request,'watchlist/movie_list.html',{'movies':movies})
+
+
+def add_movie(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        year=request.POST.get('year')
+        genre=request.POST.get('genre')
+        
+        Movie.objects.create(
+            title = title,
+            year = year if year else None,
+            genre = genre
+        )
+        
+        return redirect('movie_list')
+    return render(request,'watchlist/add_movie.html')
+
+def mark_watched(request , movie_id):
+    movie=get_object_or_404(Movie , id=movie_id)
+    movie.watched=True
+    movie.save()
+    return redirect('movie_list')
+
+def rate_movie(request , movie_id):
+    if request.method == 'POST':
+        movie = get_object_or_404(Movie , id=movie_id)
+        rating=request.POST.get('rating')
+        movie.rating=rating
+        movie.save()
+        return redirect('movie_list')
