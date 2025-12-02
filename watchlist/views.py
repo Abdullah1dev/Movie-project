a1855b3a93d8dd6203e8f68f63934fc94d262b2a
Abdullah1dev <242677@students.au.edu.pk>
2025-12-02 23:19:56 +0500

    addition of notes or reviews

--- watchlist/views.py
+++ watchlist/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render , redirect , get_object_or_404
 #now we import the models class here
 from .models import Movie
 
+
+#for movie list
 def movie_list(request):
     search_query=request.GET.get('search')
     
@@ -11,7 +13,7 @@ def movie_list(request):
         movies=Movie.objects.all()
     return render(request, 'watchlist/movie_list.html' ,{'movies' : movies} )
 
-
+#for add movie
 def add_movie(request):
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -31,17 +33,21 @@ def add_movie(request):
         return redirect('movie_list')
     return render(request,'watchlist/add_movie.html')
 
+#for mark as watched 
 def mark_watched(request , movie_id):
     movie=get_object_or_404(Movie , id=movie_id)
     movie.watched=True
     movie.save()
     return redirect('movie_list')
 
+#for rate movie
 def rate_movie(request , movie_id):
     if request.method == 'POST':
         movie = get_object_or_404(Movie , id=movie_id)
         rating=request.POST.get('rating')
+        notes=request.POST.get('notes')
         movie.rating=rating
+        movie.notes=notes
         movie.save()
         return redirect('movie_list')
     
